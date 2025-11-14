e752d42d7aa7dce0a25820790cb14e3ca5c0ab20
amresh2104 <amresh@opsz.io>
2025-11-14 15:43:12 +0530

    code updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <meta name="google-adsense-account" content="ca-pub-1961650790168883"></meta>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-1961650790168883"
+        ></meta>
         {/* ✅ Google Analytics */}
         <Script
           async
@@ -45,13 +48,11 @@ export default function RootLayout({
         </Script>
 
         {/* ✅ Google AdSense */}
-        <Script
+        <script
           async
-          id="google-adsense"
-          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1961650790168883"
           crossOrigin="anonymous"
-        />
+        ></script>
       </head>
 
       <body className={manrope.className}>
